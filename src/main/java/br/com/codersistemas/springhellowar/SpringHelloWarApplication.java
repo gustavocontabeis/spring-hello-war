235508de69db81c224400886ac13dbd9cac9ad6b
Gustavo da Silva <gustavocontabeis@gmail.com>
2020-04-15 17:02:14 -0300

    Teste para jenkins

---
+++ src/main/java/br/com/codersistemas/springhellowar/SpringHelloWarApplication.java
@@ -0,0 +1,13 @@
+package br.com.codersistemas.springhellowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/codersistemas/springhellowar/controller/HelloController.java
@@ -0,0 +1,20 @@
+package br.com.codersistemas.springhellowar.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public ResponseEntity<String> listar() {
+		return ResponseEntity.ok("OK");
+	}
+
+}
+
